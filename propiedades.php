e48f6e0b7fd35148ee0ca1571b8a56ab9879d407
OrbitalTeam <circotik@hotmail.com>
2026-07-13 20:39:59 -0500

    footer

--- propiedades.php
+++ propiedades.php
@@ -56,7 +56,7 @@ $resultado_todas = $conn->query($sql_todas);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Catálogo de Propiedades | Palomino Prime</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=<?= time(); ?>">
 </head>
 <body>
     <header>
@@ -169,7 +169,7 @@ $resultado_todas = $conn->query($sql_todas);
         </section>
     </main>
 
-    
+    <?php include 'footer.php'; ?>
 
    
 </body>
